216d2b3903d28ad802442319e7d71c7591569487
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-27 14:56:53 +0200

    made cart dynamic with redux state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,36 @@ import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 const Cart = (props) => {
+  const { items, totalAmount } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    console.log(items, totalAmount);
+  }, [items, totalAmount]);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((item) => {
+          return (
+            <CartItem
+              key={item.id}
+              item={{
+                id: item.id,
+                title: item.title,
+                total: item.totalPrice,
+                price: item.price,
+                quantity: item.quantity,
+              }}
+            />
+          );
+        })}
       </ul>
+      <h3>Total: ${totalAmount.toFixed(2)}</h3>
     </Card>
   );
 };
