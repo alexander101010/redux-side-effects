8062c4590e31ed311e1922fde88ad83f952f9505
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-27 15:43:50 +0200

    make (dummy) products dynamic with map

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,23 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
+import { DUMMY_PRODUCTS } from '../../store/dummyProducts';
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              title={product.title}
+              price={product.price}
+              description={product.description}
+              key={product.id}
+              id={product.id}
+            />
+          );
+        })}
       </ul>
     </section>
   );
