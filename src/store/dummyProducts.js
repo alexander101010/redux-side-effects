1bd45eabfed47966e6fabe46013d09d1aa7dd7d0
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-27 12:57:22 +0200

    dummy products file

---
+++ src/store/dummyProducts.js
@@ -0,0 +1,27 @@
+export const dummyProducts = [
+  {
+    id: 1,
+    price: 6,
+    title: 'Test',
+    description: 'This is a first product - amazing!',
+  },
+  {
+    id: 2,
+    price: 60,
+    title: 'Test2',
+    description: 'This is a second product - revolutionary!',
+  },
+  {
+    id: 3,
+    price: 6.66,
+    title: 'Test3',
+    description: 'This is a third product - hackneyed!',
+  },
+  {
+    id: 4,
+    price: 69,
+    title: 'Test4',
+    description:
+      "This is a fourth product - inspired! (It came around again! It's all cyclical! Time is a flat circle!)",
+  },
+];
