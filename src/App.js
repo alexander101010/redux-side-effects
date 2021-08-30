338bd71771cd73c24f5791882c6e86c720f1fc20
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-30 14:48:09 +0200

    add notification, custom action creators, isInitial state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,102 @@
+import { useEffect, Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 
-import { useSelector } from 'react-redux';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+// one way to prevent useEffect code from running on load: use a variable declared outside component
+let isInitial = true; // return when true before sending http PUT request for cart, then reassign to false so the next time code is ran, this variable won't cause the code to return before sending PUT
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  /******************************
+   * moved this code into custom action creator in cart-slice.js
+   
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch(`${FIREBASE_ROOT_URL}cart.json`, {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Cart data sent successfully.',
+        })
+      );
+
+      // const responseData = await response.json();
+      // don't actually need response data, just need to know whether response was successful or not
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed.',
+        })
+      );
+    });
+  }, [cart]);
+   **************************************/
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
